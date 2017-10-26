d7bef977a341e5c956db75c976061d42fae1321f
dev <dev@DevVM-ip179999-v12.siege.red>
2017-10-26 09:43:57 +0100

    First implementation of class ValidateLogin

---
+++ src/main/java/com/redoutevant/gitproject/ValidateLogin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redoutevant.gitproject;
+
+/**
+ *
+ * @author dev
+ */
+public class ValidateLogin {
+    
+    private String username;
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+        
+}
